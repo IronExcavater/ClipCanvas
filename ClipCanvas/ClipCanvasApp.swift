44c6b22289934145a3b9d424ee1fa1fb79035e25
IronExcavater <83278134+IronExcavater@users.noreply.github.com>
2026-05-12 21:00:55 +1000

    chore: remove Xcode template boilerplate

--- ClipCanvas/ClipCanvasApp.swift
+++ ClipCanvas/ClipCanvasApp.swift
@@ -1,32 +1,10 @@
-//
-//  ClipCanvasApp.swift
-//  ClipCanvas
-//
-//  Created by Niclas Rogulski on 12/5/2026.
-//
-
 import SwiftUI
-import SwiftData
 
 @main
 struct ClipCanvasApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Text("Loading…")
         }
-        .modelContainer(sharedModelContainer)
     }
 }
